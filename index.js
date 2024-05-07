976078f314e15bedb6fced99c5e2d33d2f0bf6c8
Alpha-lazy <anishmore712@gmail.com>
2024-05-07 18:38:24 +0530

    upated

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ const BookModel = require('./models/book.model')
 const create = async() => {
  try {
   await connectDB()
+  await BookModel.deleteMany()
+
   await BookModel.create(aulbum)
+ 
   console.log("created successfully");
  } catch (error) {
   console.log("Doesnot create");
@@ -27,9 +30,10 @@ const create = async() => {
 
 app.get("/api/song" , async(req,res)=>{
   res.status(200).json( await BookModel.find())
+
 })
 app.listen(80, async() => {
   console.log("Server is running on port  80");
-  
+    await BookModel.insertMany({name:"ANish",author:"unknown",uri:"unknown uri"},{name:"ANish2",author:"unknown2",uri:"unknown uri2"})
   await create()
 });
